bdcaf8bbcb8caf18bb37b99e99787667dd37de18
jefshandler <jef_shandler@msn.com>
2022-06-17 19:46:14 -0300

    Aula javaScript

---
+++ js/plim.js
@@ -0,0 +1,19 @@
+function myFunction() {
+  let i = 0;
+
+  for (i = 1; i <= 20; i++) {
+    let resto = 0;
+    let tipo = "impar";
+
+    resto = i % 2;
+    if (resto == 0) {
+      tipo = "par";
+    }
+    resto = i % 4;
+    if (resto == 0) {
+      document.write("plim", "-", tipo, "<br>");
+    } else {
+      document.write(i, "-", tipo, "<br>");
+    }
+  }
+}
